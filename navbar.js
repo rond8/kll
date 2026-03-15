51b4246b6cf80ac42aee16df94c4c494e9c9e84f
RD osio <125887036+rond8@users.noreply.github.com>
2026-03-15 08:21:56 +0000

    change the style

--- navbar.js
+++ navbar.js
@@ -1,10 +1,10 @@
 const navbar = `
 <header>
     <img src="schoolicon.jpg" alt="" id="schoolicon">
-   
+    <h1>Kolehiyo ng Lungsod ng Lipa</h1>
 </header>
 
-<nav>
+<nav id="mainNav" class="nav">
 <ul>
 <li><a href="index.html">HOME</a></li>
 <li><a href="about.html">ABOUT US</a></li>
@@ -18,6 +18,24 @@ const navbar = `
 
 function displaynav() {
      document.getElementById("navbar").innerHTML = navbar;
-}   
+
+     const toggle = document.getElementById('navToggle');
+     const nav = document.getElementById('mainNav');
+
+     if (toggle && nav) {
+         toggle.addEventListener('click', () => {
+             const isOpen = nav.classList.toggle('open');
+             toggle.setAttribute('aria-expanded', isOpen);
+         });
+
+         // Close menu after clicking a link (mobile)
+         nav.querySelectorAll('a').forEach(link => {
+             link.addEventListener('click', () => {
+                 nav.classList.remove('open');
+                 toggle.setAttribute('aria-expanded', 'false');
+             });
+         });
+     }
+}
 
 displaynav();
